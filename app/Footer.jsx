96955e7d997c6f72c9d0ae16c5e0ee25c417a834
Vishal <132071826+rickievishal@users.noreply.github.com>
2025-08-01 23:32:46 +0530

    Update Footer.jsx

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <div className='w-full py-[100px] bg-black text-white tracking-tighter'>
           <div className='max-w-7xl mx-auto flex flex-wrap justify-center items-center gap-[20px]'>
-          
+
+
+            
           <div className='flex flex-wrap gap-[10px] justify-center items-center gap-y-1'>
           
           <p className='text-3xl  bg-clip-text text-center bg-gradient-to-b from-gray-50 via-gray-100 to-black inline-block text-transparent'>Visit</p>
